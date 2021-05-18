007a965dd166ad34c3d58cfe2c3b5f28deaa8c34
wp9017 <airforce.edu@gmail.com>
2021-05-18 07:34:46 +0200

    Add functionality

--- app.js
+++ app.js
@@ -4,4 +4,15 @@ console.log('tests')
 let myAge = 32;
 let myHeightCm = 174;
 let myWeight = 74;
+<<<<<<< HEAD
 >>>>>>> 396f7b3... Rebase 3
+=======
+
+function myFunction(p1, p2) {
+    return p1 * p2;
+}
+
+let cars = ["BMW", "Volvo"];
+
+console.log(Math.PI);
+>>>>>>> f7bfa56... Add functionality
